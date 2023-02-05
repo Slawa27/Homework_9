c491984ac094b6838e9ce958b1f398058b896b92
Slawa27 <116310215+Slawa27@users.noreply.github.com>
2023-02-05 20:04:56 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,125 @@
+// 1 Task
+
+// let d = 7;
+
+// console.log(d);
+// console.log(d**2);
+// console.log(d**3);
+
+
+// 2 Task
+
+// let num = Number(prompt('Enter the number'));
+
+// if (num > 0){
+// 	num+=num;
+// 	console.log(num);
+// }
+// else{
+// 	console.log('The number < 0')
+// }
+
+
+//3 Task
+
+// let num1 = Number(prompt('Enter the number'));
+// let num2 = Number(prompt('Enter the number'));
+
+// if (num1 > num2){
+// 	let sum = num1 + num2;
+// 	console.log(sum);
+// }
+// else if(num1 < num2){
+// 	let sum1 = num1 * num2;
+// 	console.log(sum1);
+// }
+// else{
+// 	console.log('The numbers are identical');
+// }
+
+
+
+// 4 Task
+
+
+// let number = Number(prompt('Enter the number'));
+
+// if (number < 0){
+// 	number*=number;
+// 	console.log(number);
+// }
+// else{
+// 	console.log('Error');
+// }
+
+
+//5 Task
+
+// let students = Number(prompt('Enter the number'));
+// let chairs = Number(prompt('Enter the number'));
+
+// if (students <= chairs){
+// 	console.log('There are enough chairs');
+// }
+// else{
+// 	console.log('There are not enough chairs ');
+// }
+
+
+//6 Task
+
+// console.log('start');
+// for (let i = 1; i < 10; i++){
+// 	console.log(i);
+// }
+
+// console.log('finish');
+
+
+
+//7 Task
+
+// for (let i=5; i < 100; i++){
+// 	if (i % 5 == 0){
+// 		console.log(i);
+// 	}
+// }
+
+
+
+//8 Task
+
+// let numbers = [4, 9, 16, 18, 7, 45, 3, 55];
+
+// for (let i = 0; i < numbers.length; i++){
+// 	if (numbers[i]>=15){
+// 		console.log(numbers[i]);
+// 	}
+// }
+
+
+//9 Task
+
+// for (let i = 0; i < 100; i++){
+// 	if (i % 2 != 0){
+// 		console.log(i);
+// 	}
+// }
+
+
+
+//10 Task
+
+// let numbers = [4, 9, 16, 21, 7, 36, 12, 55, 89, 66, 101, 20, 16];
+
+// for (i = 1; i < numbers.length; i++ ){
+// 	if (i % 3 == 0){
+// 		console.log(numbers[i]);
+// 	}
+// }
+
+
+
+
+
+
